65c2fe5e293eae25c35869ea0244c88ec1eeda31
Steven Hsui <shsui5379@gmail.com>
2021-10-10 19:09:43 -0400

    Task B main

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
-
-
 #include "funcs.h"
+#include "caesar.h"
 
 int main()
 {
+  std::cout << "Task B:\n";
+
+  std::cout << "Plain text: Hello, World!" << std::endl;
+  std::cout << "Shift: 10\n";
+  std::cout << "Encrypted: " << encryptCaesar("Hello, World!", 10) << std::endl;
+
   return 0;
 }
