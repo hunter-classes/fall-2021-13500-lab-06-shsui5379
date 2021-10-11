c6415958609f740c723ac05f01d5969987ccf6b1
Steven Hsui <shsui5379@gmail.com>
2021-10-10 23:35:11 -0400

    Task C in main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "funcs.h"
 #include "caesar.h"
+#include "vigenere.h"
 
 int main()
 {
@@ -9,6 +10,14 @@ int main()
   std::cout << "Plain text: Hello, World!" << std::endl;
   std::cout << "Shift: 10\n";
   std::cout << "Encrypted: " << encryptCaesar("Hello, World!", 10) << std::endl;
+
+  std::cout << "----------------------------------------------------------------\n";
+
+  std::cout << "Task C:\n";
+
+  std::cout << "Plain text: Hello, World!" << std::endl;
+  std::cout << "Key: cake\n";
+  std::cout << "Encrypted: " << encryptVigenere("Hello, World!", "cake") << std::endl;
 
   return 0;
 }
